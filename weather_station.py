6eaf41599a96cee76f885af461705a261c63856f
AaronBraatz <aaron.braatz@haw-hamburg.de>
2025-05-11 14:28:30 +0200

    Implemented TemperatureSensor & RainGauge class
    - added init methods, also for WeatherStation
    - implemented read data method
    - implemented convert_unit method

--- weather_station.py
+++ weather_station.py
@@ -5,16 +5,35 @@ Containing parent class and som child classes with implementation for temperatur
 """
 
 class WeatherStation:
-    ...
+    def __init__(self, name: str, longitude: float, latitude: float, unit: str = "not defined"):
+        self.name = name
+        self.longitude = longitude
+        self.latitude = latitude
+        self.unit = unit
 
 
 class TemperatureSensor(WeatherStation):
+    def __init__(self, name: str, longitude: float, latitude: float):
+        super().__init__(name, longitude, latitude)
+        self.unit = "°C"
+
+    def read_data(self) -> float:
+        return self._generate_temperature_data_in_kelvin()
+
+    def convert_unit(self, value: float) -> float:
+        return value - 273.15  # Convert Kelvin to Celsius
 
     def _generate_temperature_data_in_kelvin(self) -> float:
         return 293.15  # Constant for this example
 
 
 class RainGauge(WeatherStation):
+    def __init__(self, name: str, longitude: float, latitude: float):
+        super().__init__(name, longitude, latitude)
+        self.unit = "mm"
+
+    def read_data(self) -> float:
+        return self._generate_rain_data_in_mm()
 
     def _generate_rain_data_in_mm(self) -> float:
         return 10.0  # Constant for this example
